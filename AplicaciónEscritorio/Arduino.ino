bec21f72971eb0d79fbe9933e748d8644b82cdb0
Claudia <cva1003@alu.ubu.es>
2025-05-31 16:53:58 +0200

    Arduino con desviación estandar para filtrar resultados

--- AplicaciónEscritorio/Arduino.ino
+++ AplicaciónEscritorio/Arduino.ino
@@ -10,13 +10,14 @@ float deLecturaAPeso(int x){
   float peso[]={0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5};
   int n=10;
   if (x < lectura[0] || x > lectura[n-1]) return -1.0;  // Fuera de rango
-  for (int i=0; i < n-1; i++) { // Función de interpolación lineal para calcular pesos intermedios entre los ptos declarados
+  for (int i=0; i < n-1; i++) { // Función de interpolación lineal para 
+                                //calcular pesos intermedios entre los ptos declarados.
     if (x >= lectura[i] && x <= lectura[i+1]) {
       float m=(peso[i+1]-peso[i]) / (lectura[i+1]-lectura[i]);
       return  m*(x-lectura[i])+peso[i];
-        }
+      }
     }
-    return -1.0; // Fuera de rango
+    return -1.0;// Fuera de rango
 }
 void setup() {
   pinMode(pulgar,INPUT);
@@ -25,52 +26,67 @@ void setup() {
   pinMode(anular,INPUT);
   pinMode(menique,INPUT);
   pinMode(pinBoton,INPUT);
-  pinMode(luz, OUTPUT); 
+  pinMode(luz,OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
-  if (digitalRead(pinBoton)==HIGH){
+  if (digitalRead(pinBoton) == HIGH) {
     Serial.println("Empezando a medir");
-    digitalWrite(4, HIGH);
-    int maxPulgar=0;
-    int maxIndice=0;
-    int maxCorazon=0;
-    int maxAnular=0;
-    int maxMenique=0;
-    
-    unsigned long empiece=millis();
-    while (millis()-empiece <10000){
-      maxPulgar=max(maxPulgar,analogRead(pulgar));
-      maxIndice=max(maxIndice,analogRead(indice));
-      maxCorazon=max(maxCorazon,analogRead(corazon));
-      maxAnular=max(maxAnular,analogRead(anular));
-      maxMenique=max(maxMenique,analogRead(menique));
-      delay(50);
+    digitalWrite(luz, HIGH);
+    int datosPulgar[numDatos];
+    int datosIndice[numDatos];
+    int datosCorazon[numDatos];
+    int datosAnular[numDatos];
+    int datosMenique[numDatos];
+    for (int i = 0; i < numDatos; i++) {
+      datosPulgar[i] = analogRead(pulgar);
+      datosIndice[i] = analogRead(indice);
+      datosCorazon[i] = analogRead(corazon);
+      datosAnular[i] = analogRead(anular);
+      datosMenique[i] = analogRead(menique);
+      delay(500);
     }
-    // Transforma el dato máximo recibido a Kg
-        Serial.print("Pulgar: ");
-        Serial.print(deLecturaAPeso(maxPulgar));
-        Serial.print(" Kg | ");
+    digitalWrite(luz, LOW);
 
-        Serial.print("Indice: ");
-        Serial.print(deLecturaAPeso(maxIndice));
-        Serial.print(" Kg | ");
+    filtrado("Pulgar",datosPulgar,numDatos);
+    filtrado("Indice",datosIndice,numDatos);
+    filtrado("Corazon",datosCorazon,numDatos);
+    filtrado("Anular",datosAnular,numDatos);
+    filtrado("Menique",datosMenique,numDatos);
+    delay(2000);
+  } 
+}
 
-        Serial.print("Corazon: ");
-        Serial.print(deLecturaAPeso(maxCorazon));
-        Serial.print(" Kg | ");
-
-        Serial.print("Anular: ");
-        Serial.print(deLecturaAPeso(maxAnular));
-        Serial.print(" Kg | ");
-
-        Serial.print("Menique: ");
-        Serial.print(deLecturaAPeso(maxMenique));
-        Serial.println(" Kg");
-
-        delay(2000);
-    }
-    else {
-        digitalWrite(4, LOW);
-  		}
+void filtrado(String nombre, int datos[], int total){
+  float suma = 0;
+  for (int i = 0; i < total; i++){
+    suma += datos[i];
   }
+  float media = suma/total;
+  // Calcula desviación estandar para eliminar datos atipicos
+  float sumaDesviacion = 0;
+  for (int i = 0; i < total; i++){
+    sumaDesviacion += pow(datos[i] - media, 2);
+  }
+  float desviacion = sqrt(sumaDesviacion/total);
+
+  float limiteInferior = media - desviacion;
+  float limiteSuperior = media + desviacion;
+
+  float sumaFiltrada = 0;
+  int contadorFiltrados = 0;
+
+  for (int i = 0; i < total; i++){
+    if (datos[i] >= limiteInferior && datos[i] <= limiteSuperior){
+      sumaFiltrada += datos[i];
+      contadorFiltrados++;
+    }
+  }
+  float mediaFiltrada = (sumaFiltrada/contadorFiltrados);
+  float kg = deLecturaAPeso(mediaFiltrada);
+
+  Serial.print(nombre);
+  Serial.print(": ");
+  Serial.print(kg);
+  Serial.print(" Kg | ");
+}
